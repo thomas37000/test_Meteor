7794f474772f6df2e4e90739d09c6085df50ff19
Thomas <thomas@avecnous.eu>
2021-06-11 18:04:40 +0200

    test a simple map

--- imports/ui/Info.jsx
+++ imports/ui/Info.jsx
@@ -4,16 +4,22 @@ import { TasksCollection } from '../api/TasksCollection';
 import { Task } from './Task';
 
 export const Info = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  // const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  
+  const tasks = [
+    {_id: 1, text: 'First Task'},
+    {_id: 2, text: 'Second Task'},
+    {_id: 3, text: 'Third Task'},
+  ];
 
   return (
     <div>
       <h2>Learn Meteor!</h2>
-      {/* <ul>
+      <ul>
         {tasks.map((task) => (
           <Task key={task._id} task={task} />
         ))}
-      </ul> */}
+      </ul>
     </div>
   );
 };
--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { TasksCollection } from '/imports/api/TasksCollection';
 
 const insertTask = taskText => TasksCollection.insert({ text: taskText });
- 
+  
 Meteor.startup(() => {
   if (TasksCollection.find().count() === 0) {
     [
