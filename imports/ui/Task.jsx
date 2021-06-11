3883e1f7dbde34ea3cf5e8a7335ce7e3f3237f13
Thomas <thomas@avecnous.eu>
2021-06-11 18:01:44 +0200

    delete 2 props

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Task = ({ task }) => {
   return (
     <li>
-      {task.text} {task.title} {task.url}
+      {task.text}
     </li>
   );
 };
--- server/main.js
+++ server/main.js
@@ -1,8 +1,8 @@
 import { Meteor } from 'meteor/meteor';
-import { TasksCollection } from '../imports/api/TasksCollection';
+import { TasksCollection } from '/imports/api/TasksCollection';
 
-const insertTask = (taskText) => TasksCollection.insert({ text: taskText });
-
+const insertTask = taskText => TasksCollection.insert({ text: taskText });
+ 
 Meteor.startup(() => {
   if (TasksCollection.find().count() === 0) {
     [
@@ -12,7 +12,7 @@ Meteor.startup(() => {
       'Fourth Task',
       'Fifth Task',
       'Sixth Task',
-      'Seventh Task',
-    ].forEach(insertTask);
+      'Seventh Task'
+    ].forEach(insertTask)
   }
 });
